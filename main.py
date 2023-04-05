0b36ebad513c9363c1aa0f5a4c30235fc5ecde8c
radhiasdf <radhityarr@gmail.com>
2023-04-06 09:14:34 +1200

    added ability to add, inserting CSV to database is generalised

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
 import sqlite3
 import csv
 import tabulate as tab
-import itertools
+
+reset = True
 
 backkey = "<"
+
+connect = sqlite3.connect('Database.db')
+c = connect.cursor()
 
 
 def update():
 	print("-Enter '" + backkey + "' to go back-")
 
 
-# Inserting data from the csvs to the databases
-def insertCSVs():
-	with open("13 Databases data (1) - Classes.csv", "r") as file:
-		for row in csv.reader(file):
-			c.execute("""INSERT INTO Classes (ID,Name,YearLevel,TeacherID) 
-						VALUES (?, ?, ?, ?);""", (row[0], row[1], row[2], row[3]))
-
-	with open("13 Databases data (1) - Students.csv", "r") as file:
-		for row in csv.reader(file):
-			c.execute("""INSERT INTO Students (ID,first_name,last_name,YearLevel) 
-						VALUES (?, ?, ?, ?);""", (row[0], row[1], row[2], row[3]))
-
-	with open("13 Databases data (1) - Students and Classes.csv", "r") as file:
-		for row in csv.reader(file):
-			c.execute("""INSERT INTO StudentsAndClasses (StudentID,ClassID) 
-						VALUES (?, ?);""", (row[0], row[1]))
-
-	with open("13 Databases data (1) - Students with timetables.csv", "r") as file:
-		for row in csv.reader(file):
-			c.execute("""INSERT INTO StudentsWithTimetables (first_name,last_name,YearLevel,Subject1,Subject2,Subject3,Subject4,Subject5,Subject6) 
-						VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);""", (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
-
-	with open("13 Databases data (1) - Teachers.csv", "r") as file:
-		for row in csv.reader(file):
-			c.execute("""INSERT INTO Teachers (ID,FirstName,LastName) 
-						VALUES (?, ?, ?);""", (row[0], row[1], row[2]))
-
-
-connect = sqlite3.connect('Database.db')
-c = connect.cursor()
-
 # This sets the thing
-reset = True
-if reset:
+def resetTables():
 	c.execute("DROP TABLE IF EXISTS 'Classes';")
 	c.execute("DROP TABLE IF EXISTS 'Students';")
 	c.execute("DROP TABLE IF EXISTS 'StudentsAndClasses';")
@@ -98,18 +70,21 @@ if reset:
 		LastName TEXT NOT NULL
 	);
 	""")
-	insertCSVs()
 	connect.commit()
 
-c.execute("""SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';""")
-tables = c.fetchall()
+
+def CSVtoDatabase(filename, tablename):
+	with open(filename, "r") as file:
+		columnNames = ','.join(next(csv.reader(file)))
+		for i, row in enumerate(csv.reader(file)):
+			if i > 0:
+				c.execute(f"""INSERT INTO {tablename} ({columnNames}) 
+							VALUES ({('?,' * len(row))[:-1]});""", tuple(row))
 
 
 def main():
 	while True:
-		search()
-	while True:
-		action = input("hey do you want to search something (1) or add something (2): ")
+		action = input("(Enter '<' to go back)\nhey do you want to search something (1) or add something (2): ")
 		try:
 			if int(action) == 1:
 				search()
@@ -134,44 +109,86 @@ def search():
 
 	while True:
 		try:
-			selected = input("hey lets enter some specific columns you want to search stuff in, eg. 1,2,3: ")
+			selected = inp = input("hey lets enter some specific columns you want to search stuff in, eg. 1,2,3: ")
+			if inp == backkey:
+				return
 			columns = [columnDict[int(i)] for i in selected.split(",")]
 			print(', '.join(columns))
 			selectedtables = [column.split('.', 1)[0] for column in columns]
-			print(selectedtables)
 			break
 		except ValueError:
 			pass
 		except KeyError:
 			pass
+
 	joinings = ""
+	fromm = ""
 	if 'Students' in selectedtables and 'Classes' in selectedtables:
+		fromm = "FROM StudentsAndClasses"
 		joinings += """
-		FROM (StudentsAndClasses)
 		INNER JOIN Students ON Students.ID = StudentsAndClasses.StudentID
 		INNER JOIN Classes ON Classes.ID = StudentsAndClasses.ClassID"""
 	if 'Teachers' in selectedtables and 'Classes' in selectedtables:
+		fromm = "FROM Classes"
 		joinings += """
-		FROM (Classes)
 		INNER JOIN Teachers ON Teachers.ID = Classes.TeacherID"""
+	if fromm == "":
+		fromm = f"FROM {selectedtables[0]}"
 
 	orderings = "ORDER BY"
 	for column in columns:
 		orderings += f" {column} ASC,"
 	orderings = orderings[:-1]
-	print(f"""SELECT {', '.join(columns)}
-			{joinings}
-			{orderings};
-			""")
-	try:
-		c.execute(f"""SELECT {', '.join(columns)} {joinings} {orderings};""")
-		print("\n" + tab.tabulate(c.fetchall(), columns, tablefmt='simple'))
-	except sqlite3.OperationalError:
-		print("hey either too many relationships or theres no direct relationship")
+	query = f"""SELECT {', '.join(columns)} {fromm} {joinings} {orderings};"""
+	print(query)
+	c.execute(query)
+	print("\n" + tab.tabulate(c.fetchall(), columns, tablefmt='simple'))
 
 
 def add():
-	pass
+	for i, table in enumerate(tables):
+		print(f"{i + 1}. {table[0]}")
+	while True:
+		try:
+			inp = input("hey which table do you want to add in: ")
+			if inp == backkey:
+				return
+			num = int(inp) - 1
+			break
+		except ValueError:
+			pass
+		except IndexError:
+			pass
+	while True:
+		c.execute(f"SELECT * FROM {tables[num][0]};")
+		display = c.fetchall()
+		c.execute(f"PRAGMA table_info({tables[num][0]});")
+		headers = [column[1] for column in c.fetchall()]
+		print("\n" + tab.tabulate(display, headers, tablefmt='simple'))
 
+		headersNoID = [data for data in headers if data != 'ID']
+		inp = input(f"hey now enter the things in the format {','.join(headersNoID)}: ")
+		if inp == backkey:
+			return
+		data = inp.split(',')
+		if len(data) != len(headersNoID):
+			print("hey number of columns doesnt match")
+			continue
+		query = f"""INSERT INTO {tables[num][0]} ({','.join(headersNoID)}) 
+								VALUES ({('?,' * len(headersNoID))[:-1]});"""
+		c.execute(query, tuple(data))
+		connect.commit()
+
+
+if reset:
+	resetTables()
+	CSVtoDatabase("13 Databases data (1) - Classes.csv", "Classes")
+	CSVtoDatabase("13 Databases data (1) - Students.csv", "Students")
+	CSVtoDatabase("13 Databases data (1) - Students and Classes.csv", "StudentsAndClasses")
+	CSVtoDatabase("13 Databases data (1) - Students with timetables.csv", "StudentsWithTimetables")
+	CSVtoDatabase("13 Databases data (1) - Teachers.csv", "Teachers")
+
+c.execute("""SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';""")
+tables = c.fetchall()
 
 main()
